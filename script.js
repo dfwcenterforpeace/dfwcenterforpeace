38069aad00f08ce8f23d80a3859a01bf8c6e972d
part1course <149856006+part1course@users.noreply.github.com>
2025-02-07 01:18:53 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const contentJsonUrl = "contentA.json"; //Change it after testing the application to content.json
+const contentJsonUrl = "contentB.json"; //Change it after testing the application to content.json
 let contentList = [];
 let allContentIndex = 0;
 let recordIndex = 0;
